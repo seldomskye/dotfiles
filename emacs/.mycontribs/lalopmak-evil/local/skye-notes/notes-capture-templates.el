fe0a97ce54d745850e0fd8c1909dcbb43f753a4c
Skye <skye@skye.moe>
2017-10-14 12:28:58 -0400

    Unify personal/work capture templates

--- emacs/.mycontribs/lalopmak-evil/local/skye-notes/notes-capture-templates.el
+++ emacs/.mycontribs/lalopmak-evil/local/skye-notes/notes-capture-templates.el
@@ -2,9 +2,14 @@
         `(("n" "Note" entry
            (file+olp "~/Dropbox (HubSpot)/work.org" "Inbox")
            )
-          ("t" "Todo" entry
+          ("t" "todos")
+          ("tw" "Work Todo" entry
            (file+olp "~/Dropbox (HubSpot)/work.org" "Inbox")
            "* TODO %?\n")
+          ("tp" "Personal Todo" entry
+           (file+olp "~/org/zettel.org" "Inbox")
+           "* TODO %?\n%U\n%a\n"
+           )
           ("g" "Git Issue" entry
            (file+olp "~/Dropbox (HubSpot)/work.org" "Dev Projects")
            "* TODO [[https://git.hubteam.com/HubSpot/AutomationTeam/issues/%\\1][Issue %^{Issue Number?}]]\n"
@@ -13,11 +18,12 @@
            (file+olp "~/Dropbox (HubSpot)/work.org" "Jira Tickets")
            "* TODO [[https://issues.hubspotcentral.com/browse/WORKFLOWS-%\\1][Ticket %^{Ticket Number?}]]\n"
            )
+          ("p" "Personal Programming" entry
+           (file+olp "~/org/zettel.org" "Inbox")
+           "* %^{Title}\n"
+           )
+          
           )
-        
         )
-
-(setq org-todo-keywords
-      '((sequence "BLOCKED" "TODO" "DONE")))
 
 (provide 'notes-capture-templates)
--- emacs/.mycontribs/lalopmak-evil/packages.el
+++ emacs/.mycontribs/lalopmak-evil/packages.el
@@ -3,18 +3,30 @@
         (skye-notes :location local)
         (sync-window :location local)
 ))
-
+(defun lalopmak-evil/set-agenda-commands () 
+  (setq org-agenda-custom-commands '(
+                                       ("a" "All agenda items" agenda "Agenda"
+                                         ((org-agenda-start-on-weekday nil))
+                                         ("~/org/agenda.txt"))
+                                        ("w" "Work agenda"
+                                         ((agenda ""
+                                                  ((org-agenda-category-filter-preset
+                                                    (quote
+                                                     ("+work"))))))
+                                         nil)
+                                        ("p" "Personal"
+                                         ((alltodo ""
+                                                   ((org-agenda-category-filter-preset
+                                                     (quote
+                                                      ("+zettel"))))))
+                                         nil nil)
+                                        )
+                                       ))
+(lalopmak-evil/set-agenda-commands)
 (defun lalopmak-evil/init-skye-notes ()
   (use-package skye-notes
     :init
-    (progn (setq org-agenda-custom-commands '(
-                                              ("t" alltodo "All TODOs"
-                                               ((org-agenda-files '("~/org/zettel.org")))
-                                               ("~/org/todos.txt"))
-                                               ("a" agenda "Agenda" 
-                                                ((org-agenda-start-on-weekday nil))
-                                                ("~/org/agenda.txt"))
-                                           )))
+    (progn (lalopmak-evil/set-agenda-commands))
     :config
     (progn
       (spacemacs/set-leader-keys-for-major-mode 'org-mode 
